131ff75b9aa229439b282a30d1a01823ad6495b9
Nacho Castillo <4811095288ic@gmail.com>
2018-06-08 11:41:16 -0500

    Testing

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const secure=require('express-force-https');
+// const secure=require('express-force-https');
 const serveStatic = require('serve-static');
-const history=require('connect-history-api-fallback');
+// const history=require('connect-history-api-fallback');
 
 
 
-app.use(history());
+// app.use(history());
 
 // app.use(express.static(path.join(__dirname,'public')));
 app.use(serveStatic(path.join(__dirname,'public')));
