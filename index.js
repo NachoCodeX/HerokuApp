1a978da409e5e3ac9776284551b8e99bb6b882a8
Nacho Castillo <4811095288ic@gmail.com>
2018-06-08 11:20:29 -0500

    Test

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
+const app = require('./app')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const server = require('http').Server(app);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+server.listen(5000,'0.0.0.0',e=>{
+  if(e) process.exit(1);
+  console.log(`Server on PORT ${PORT}`);
+});
+
+
+
+
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
