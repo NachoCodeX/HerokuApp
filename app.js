733f82d0668284edbec3c0faaf33d672eb48866e
Nacho Castillo <4811095288ic@gmail.com>
2018-06-08 11:26:28 -0500

    testing

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const history=require('connect-history-api-fallback');
 app.use(history());
 
 // app.use(express.static(path.join(__dirname,'public')));
-app.use(serveStatic(path.join(__dirname,'public','pwa-mat')));
+app.use(serveStatic(path.join(__dirname,'public')));
 
 
 
 
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'public','pwa-mat','index.html')));
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'public','index.html')));
 
 
 
